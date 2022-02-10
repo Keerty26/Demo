fb9fa7eead2cda11a2c370a4e2101329cb5dab10
Keerty26 <99384482+Keerty26@users.noreply.github.com>
2022-02-10 12:40:42 +0530

    Created Sample2.java

---
+++ Sample2.java
@@ -0,0 +1,12 @@
+class Sample2{
+  public static void main(String[] args){
+    String str="Java File";
+    String rev="";
+    String[] arr=str.split(" ");
+    for(int i=arr.length-1; i>=0; i--){
+      String word=arr[i];
+      rev+=word+" ";
+    }
+    System.out.println(rev.trim());
+  }
+}
